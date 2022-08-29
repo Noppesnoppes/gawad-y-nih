6d807ef6687de5f5692f8a5a27ffca106d6a15d6
Noah Månsson Persson <noah.manssonpersson@learnet.se>
2022-08-29 13:11:15 +0200

    lektion 3

---
+++ test.js
@@ -0,0 +1,14 @@
+//console.log("hej") 
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("Hello, how are you?: ");
+// Write your code here:
+let name2 = prompt("That's interesting, tell me more");
+
+// Write your code here:
+console.log("thank you for sharing")
+
+
+
